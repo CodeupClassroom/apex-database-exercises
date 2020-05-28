5a48a44fd9a4927aa9a40310639d2b2a77f65abb
Fer <fernandoza@gmail.com>
2020-05-28 16:29:17 -0500

    feat: add a simple relationship with a FK and PK

--- demo.sql
+++ demo.sql
@@ -1,13 +1,14 @@
 USE apex_db;
 
-drop table if exists authors;
 drop table if exists quotes;
+drop table if exists authors;
 
 create table if not exists authors
 (
     id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
     first_name varchar(255) not null,
     last_name  varchar(255) not null,
+    unique (first_name, last_name),
     primary key (id)
 );
 
@@ -15,7 +16,7 @@ CREATE TABLE IF NOT EXISTS quotes
 (
     id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
     author_id int unsigned not null,
-    content   VARCHAR(240) NOT NULL,
+    content   VARCHAR(240) NOT NULL unique,
     PRIMARY KEY (id),
     foreign key (author_id) references authors (id)
 );

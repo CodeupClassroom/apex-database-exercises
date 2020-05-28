170c8bb10c164a2f9e99e389831a92fa675aa921
Fer <fernandoza@gmail.com>
2020-05-28 11:18:07 -0500

    feat: examples with time and dates

--- functions-lecture.sql
+++ functions-lecture.sql
@@ -17,3 +17,14 @@ SELECT distinct first_name
 FROM employees
 WHERE first_name NOT LIKE '%a%'
 order by first_name;
+
+select now() AS 'today';
+select curdate();
+select curtime();
+
+select UNIX_TIMESTAMP();
+SELECT CONCAT(
+               'Teaching people to code for ',
+               ((((UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2014-02-04')) / 60) / 60) /24) / 365,
+               ' years'
+           );

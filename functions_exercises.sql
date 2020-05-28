7f5cececd623659aa8f5472421542206503679c8
Fer <fernandoza@gmail.com>
2020-05-28 14:19:42 -0500

    feat: add a mysql8 solution

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -40,23 +40,9 @@ WHERE last_name LIKE 'E%';
 
 
 
-
-
-
-
-
-
-
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
-
-
-
-
-
-
-
 
 
 
@@ -66,14 +52,9 @@ WHERE birth_date LIKE '%12-25';
 
 
 
-
-
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
-
-
-
 
 
 -- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
@@ -82,8 +63,6 @@ FROM employees
 WHERE first_name = 'Irena'
    OR first_name = 'Vidya'
    OR first_name = 'Maya';
-
-
 
 
 -- Add a condition to the previous query to find everybody with those names who is also male
@@ -95,25 +74,26 @@ WHERE (first_name = 'Irena'
   AND gender = 'M';
 
 
-
 -- Find all employees whose last name starts or ends with 'E'
 SELECT emp_no, birth_date, concat_ws(' ', first_name, last_name) as 'full_name', gender, hire_date
 FROM employees
-WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E';
 
 
 -- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
 
 
 -- Find all employees hired in the 90s and born on Christmas
 SELECT *, datediff(curdate(), hire_date) as 'days working in the company'
 FROM employees
-WHERE hire_date LIKE '199%'
-  AND birth_date LIKE '%12-25';
+WHERE year(hire_date) between 1990 and 1999
+  AND month(birth_date) = 12
+  and day(birth_date) = 25;
 
 
 -- Find all employees with a 'q' in their last name but not 'qu'
